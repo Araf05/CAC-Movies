1d341c4899d7c61fc079d5baa377bb5be419ee9d
Nasherc <nashercabrera@hotmail.com>
2024-06-06 20:50:31 -0300

    api

--- js/api.js
+++ js/api.js
@@ -8,34 +8,46 @@ async function api()
     }
     catch (error)
     {
-
+        throw new error("Hay un error de conexion", error)
     }
 }
+
+
+let divNuevo = ""
+
 console.log("andando 1")
 function nuevoUsuario(usuario)
 {
     let div= document.getElementById("api")
-    let divNuevo= document.createElement("div")
+    divNuevo= document.createElement("div")
 
     divNuevo.innerHTML=
-    `   <img src="${usuario.picture.medium}"></img>
+    `   <img src="${usuario.picture.large}"></img>
         <p>Nombre: ${usuario.name.first}</p>
         <p>Apellido: ${usuario.name.last}</p>`
     div.appendChild(divNuevo)
 
 }
 
+
 async function imprimir()
 {
+    try
+    {
+        for (let i=0;i<6;i++)
+                    
+            {
+                
+                let usuario = await api()
+                nuevoUsuario(usuario)
+    
+            }
+    }
+    catch (error)
+    {
+        throw new error ("Hay un error de conexion", error)
+    }
 
-    for (let i=0;i<3;i++)
-        {
-            console.log("andando 2")
-    
-            let usuario= await api()
-            nuevoUsuario(usuario)
-    
-        }
 }
 
 imprimir()
